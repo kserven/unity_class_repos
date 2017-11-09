19a72e3daa29a8e1effc84180c6bee9a77f4b1f4
Keith Serven <kserven@gmail.com>
2017-11-08 17:00:53 -0800

    add random color to sight words

--- Unity/Sight Words K/Assets/Main.cs
+++ Unity/Sight Words K/Assets/Main.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Main : MonoBehaviour {
+public class Main : MonoBehaviour
+{
 
-    public Text text;
+    public Text text, color;
     public int item = 0;
 
     string[] words = new string[] { "I", "a", "am", "at", "the", "see", "had", "as", "and", "it", "in", "is",
@@ -13,26 +14,63 @@ public class Main : MonoBehaviour {
                                     "what", "you", "we", "up", "she", "him", "her", "his", "look", "for", "boy",
                                     "girl", "said", "they", "with", "but", "of", "little", "then", "be", "were",
                                     "that", "down", "some", "there", "out", "when"
-    };
+                                    };
 
-	// Use this for initialization
-	void Start () {
-        text.text = words[item];	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start()
+    {
+        text.text = words[item];
+    }
 
-        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.RightArrow)) && item < 49) {
+    // Update is called once per frame
+    void Update()
+    {
+
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.RightArrow)) && item < 49)
+        {
             item++;
+            RandColor();
         }
-        else if (Input.GetKeyDown(KeyCode.LeftArrow) && item > 0) {
+        else if (Input.GetKeyDown(KeyCode.LeftArrow) && item > 0)
+        {
             item--;
+            RandColor();
         }
-        else if (Input.GetKeyDown(KeyCode.Q) || Input.GetKeyDown(KeyCode.Escape)) {
+        else if (Input.GetKeyDown(KeyCode.Q) || Input.GetKeyDown(KeyCode.Escape))
+        {
             Application.Quit();
         }
 
         text.text = words[item];
-	}
+    }
+
+    void RandColor()
+    {
+        float textColor;
+        textColor = Mathf.Round((Random.value) * 5);
+        if (textColor == 0)
+        {
+            text.color = Color.red;
+        }
+        else if (textColor == 1)
+        {
+            text.color = Color.blue;
+        }
+        else if (textColor == 2)
+        {
+            text.color = Color.green;
+        }
+        else if (textColor == 3)
+        {
+            text.color = Color.yellow;
+        }
+        else if (textColor == 4)
+        {
+            text.color = Color.cyan;
+        }
+        else
+        {
+            text.color = Color.magenta;
+        }
+    }
 }
